ec26a9f71fbc1fffc218ffd8514d8c7ad7f9794e
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2021-03-14 10:32:52 +0700

    update: undestanding why we use className not just class and added css style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My Todo</h1>
+      <div className="card">
+        <h2>Title</h2>
+        <div className="action">
+          <button className="btn">Delete</button>
+        </div>
+      </div>
     </div>
   );
 }
