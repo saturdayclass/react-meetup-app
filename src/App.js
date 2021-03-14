c39a12a4513742aaa3be230794d40e861eb6254b
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2021-03-14 10:45:21 +0700

    update: added props

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ function App() {
   return (
     <div>
       <h1>My Todo</h1>
-      <Todo />
-      <Todo />
+      <Todo text="learn react" />
+      <Todo text="master react" />
+      <Todo text="advance react" />
     </div>
   );
 }
